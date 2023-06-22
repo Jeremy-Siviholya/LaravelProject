3e74e1e6804efe90babf0d25d8e63d1dcb287daf
Jeremy <129526295+Jeremy-Siviholya@users.noreply.github.com>
2023-06-22 12:44:15 +0200

    initialize laravel project

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about',function(){
+    return view('about');
+});
+
+Route::get('/contact',function(){
+    return view('contact');
+});
+Route::get('/services',function(){
+    return view('services');
+});
+Route::get('/login',function(){
+    return view('login');
+});
